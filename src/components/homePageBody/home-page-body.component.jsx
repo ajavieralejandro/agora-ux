00544d65017b8ae27803738964047ed9ed717381
ajavieralejandro <amorosijavier@gmail.com>
2020-02-22 22:28:14 -0300

    new idea for homepagebody

--- src/components/homePageBody/home-page-body.component.jsx
+++ src/components/homePageBody/home-page-body.component.jsx
@@ -1,18 +1,25 @@
 import React from 'react';
 import Grid from '@material-ui/core/Grid';
 import Member from '../member/member.component';
-import {Wrap} from './home-page-body.style';
+import {Wrap,Title,StyledImage} from './home-page-body.style';
 import SearchButton from '../search/search.component';
+import { FaDelicious } from "react-icons/fa";
+
 import Paper from '@material-ui/core/Paper';
 
 
 const HomePageBody = () => {
     return(
         <Wrap>
+        <Title>Agora3D</Title>
+        <FaDelicious size={44} />
+        <p>Escuela lationamericana de biomodelos</p>
         <Grid container spacing={4}>
         <Grid item xs={12} md={4}> 
         <h1>Hola soy una prueba</h1>
-        <SearchButton />
+        <hr />
+        <h1>Nuestro Equipo</h1>
+        <hr />
         </Grid> <Grid item xs={12} md={8} container
         direction="row"
         justify="center"
@@ -20,12 +27,9 @@ const HomePageBody = () => {
         spacing={1}
       >
 
-      <Grid container spacing={1}>
-      <Grid item md={4} ><Member /></Grid>
-      <Grid item  md={4}><Member /></Grid>
-      <Grid item  md={4}><Member /></Grid>   
-      </Grid>
-      
+      <StyledImage src="https://cdn.computerhoy.com/sites/navi.axelspringer.es/public/styles/1200/public/media/image/2015/11/131579-objetos-mas-sorprendentes-hechos-impresora-3d.jpg?itok=KNNEPm_Z" alt="Italian Trulli" />
+
+
      
         </Grid>
 
--- src/components/homePageBody/home-page-body.style.jsx
+++ src/components/homePageBody/home-page-body.style.jsx
@@ -1,8 +1,24 @@
 import styled, { css } from "styled-components";
-
+import { FaDelicious } from "react-icons/fa";
 
 export const Wrap = styled.div`
+text-align: center;
 
     margin-top:5%;
 
+`
+
+export const Title = styled.div
+ `
+    text-align: center;
+    font-size: 55px;
+`
+
+export const DeliciousIcon  = styled(FaDelicious)`
+    font-size: 22ps;
+`
+
+export const StyledImage = styled.img`
+    max-width: 79%;
+    max-heigth:  90%;
 `
