f0255321bf538547862496d7e0fa33f5829e15fe
ajavieralejandro <amorosijavier@gmail.com>
2020-02-25 14:37:02 -0300

    homepage refactoring...

--- src/pages/homepage/homepage.page.jsx
+++ src/pages/homepage/homepage.page.jsx
@@ -1,61 +1,30 @@
 import React from 'react';
 //Components
-import Grid from '@material-ui/core/Grid';
-import SearchIcon from '@material-ui/icons/Search';
-import IconButton from '@material-ui/core/IconButton';
-import TextField from '@material-ui/core/TextField';
 import SearchButton from '../../components/search/search.component';
-import InputAdornment from '@material-ui/core/InputAdornment';
-import AccountCircle from '@material-ui/icons/AccountCircle';
-import ParallaxDiv from '../../components/parallaxDiv/parallax-div.container';
-import Paper from '@material-ui/core/Paper';
-import Member from '../../components/member/member.component';
 import Container from '@material-ui/core/Container';
 import HomePageBody from '../../components/homePageBody/home-page-body.component';
-import Button from '@material-ui/core/Button';
-
-
-
-
-
-
-
-import Navbar from "../../components/navbar/navbar.component";
 import { Parallax} from 'react-parallax';
-import CourseCard from "../../components/courseCard/course-card.component";
-
 //Styles
-import './homepage.styles.scss';
-import CourseImages from '../../components/courseImages/course-images.component';
+import { InsideParallaxDiv, ParallaxImageDiv, Header } from './homepage.styles';
 
 const HomePage  = (props) => {
   return(
       <div id="page-wrapper">
-      <header className="App-header">
-        {/* -----basic config-----*/}
-      
-      <Parallax
-     
-      blur={{ min: -45, max: 15 }}
-      bgImage="https://lh3.googleusercontent.com/-vFnUrApIvRA/XlGwaSJjjII/AAAAAAAAFhg/ONyr3sahLfQnRsM1ddJShn2Yf1n1MR_9ACK8BGAsYHg/s0/2020-02-22.png"
-      bgImageAlt="the dog"
-      strength={500}
-  >
+        <Header>      
+        <Parallax
+        blur={{ min: -45, max: 15 }}
+        bgImage="https://lh3.googleusercontent.com/-vFnUrApIvRA/XlGwaSJjjII/AAAAAAAAFhg/ONyr3sahLfQnRsM1ddJShn2Yf1n1MR_9ACK8BGAsYHg/s0/2020-02-22.png"
+        bgImageAlt="the dog"
+        strength={500}>
+        <ParallaxImageDiv />
   
-      <div style={{ height: '800px', width:'600px' }} />
-      <div className="centered">
-        <h1>mucho más que una escuela online</h1>
-        <SearchButton />
-       
-      </div>
-     
-      </Parallax>
+          <InsideParallaxDiv>
+            <h1>mucho más que una escuela online</h1>
+            <SearchButton />
+          </InsideParallaxDiv>
+        </Parallax>
 
-
-     
-  
-        
-      </header>
+      </Header>
       <Container>
       <HomePageBody />
       </Container>
---
+++ src/pages/homepage/homepage.styles.jsx
@@ -0,0 +1,35 @@
+import styled from "styled-components";
+
+export const ParallaxImageDiv =  styled.div`
+    height: 800px;
+    width: 600px; 
+`
+
+export const Header = styled.header`
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    font-size: calc(10px + 2vmin);
+    color: black;
+    width: 100%;
+    margin:0;
+
+`
+
+
+export const InsideParallaxDiv = styled.div`
+    position: absolute;
+    bottom: 170px;
+    left: 146px;
+    color: blanchedalmond;
+    font-family: 'Barlow', sans-serif;
+    font-size: 18px;
+    @media (max-width: 900px) {
+        
+          position: absolute;
+          top: 50%;
+          left: 50%;
+          transform: translate(-50%, -50%);
+        
+      }
+`
