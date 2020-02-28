a783dd057e991b50c135d08204c6dbd63c5baa3c
ajavieralejandro <amorosijavier@gmail.com>
2020-02-27 23:07:05 -0300

    iconButton HOC refactoring

---
+++ src/components/IconButton/icon-button.component.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import IconButton from '@material-ui/core/IconButton';
+const IconButton = (label,icon) =>{
+    <IconButton aria-label={label}>
+        {icon}
+    </IconButton>
+}
+export const IconButton
--- src/components/footer/footer.component.jsx
+++ src/components/footer/footer.component.jsx
@@ -1,15 +1,28 @@
 import React from 'react';
+import Grid from '@material-ui/core/Grid';
+import Container from '@material-ui/core/Container';
+
 import {FooterDiv,FooterContainer,BackDiv} from './footer.styles';
 const Footer = () =>{
     return(
         <BackDiv>
-        <FooterDiv>
+            <FooterDiv>
+            <Container>
 
-  <p>Posted by: Hege Refsnes</p>
-  <p>Contact information: <a href="mailto:someone@example.com">
-  someone@example.com</a>.</p>
-  </FooterDiv>
-  </BackDiv>
+                <Grid container spacing={1}>
+                    <Grid container item xs={12} md={4} spacing={3}>
+                    <h1>hola soy una prueba</h1>
+                    </Grid>
+                    <Grid container item xs={12} md={4}  spacing={3}>
+                    <h1>hola soy una prueba</h1>
+                    </Grid>
+                    <Grid container item xs={12} md={4}  spacing={3}>
+                    <h1>hola soy una prueba</h1>
+                    </Grid>                
+                </Grid>
+                </Container>    
+        </FooterDiv>
+        </BackDiv>
 
     )
 }
