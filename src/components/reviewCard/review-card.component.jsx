0d7faec85c1e590632a5b67f519e0deec7be2b44
ajavieralejandro <amorosijavier@gmail.com>
2020-04-15 02:29:05 -0300

    changes to the cardReview

--- src/components/reviewCard/review-card.component.jsx
+++ src/components/reviewCard/review-card.component.jsx
@@ -7,15 +7,20 @@ import CardActions from "@material-ui/core/CardActions";
 import CardContent from "@material-ui/core/CardContent";
 import Button from "@material-ui/core/Button";
 import Hidden from "@material-ui/core/Hidden";
-import { Wrap } from "./review-card.styles";
+import { Wrap, Response, RightButton } from "./review-card.styles";
 import { FiMessageSquare } from "react-icons/fi";
+import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
 
 const ReviewCard2 = () => {
-  const [Comments, setComments] = useState(true);
-  const handleClick = () => {
-    console.log("Estoy atajando bien el click");
+  const [Comments, setComments] = useState(false);
+  const [Content, setContent] = useState(false);
+
+  const handleComments = () => {
     setComments(!Comments);
-    console.log(Comments);
+  };
+
+  const handleClick = () => {
+    setContent(!Content);
   };
 
   return (
@@ -32,9 +37,18 @@ const ReviewCard2 = () => {
                   sesión iniciada
                 </p>
 
-                {Comments ? null : (
-                  <p>Hola yo se supone que estoy escondido viste</p>
-                )}
+                {Content ? (
+                  <div>
+                    <h4>Admin</h4>
+                    <p>Hola yo se supone que estoy escondido viste</p>
+                  </div>
+                ) : null}
+                {Comments ? (
+                  <Response>
+                    <h3>Admin</h3>
+                    <p>Hola soy una respuesta</p>
+                  </Response>
+                ) : null}
               </Container>
             </Grid>
             <Grid container item xs={2}>
@@ -48,8 +62,11 @@ const ReviewCard2 = () => {
               <Grid item xs={10}>
                 <Button onClick={handleClick} size="small">
                   {" "}
-                  <FiMessageSquare size={27} />
+                  <ExpandMoreIcon />
                 </Button>
+                <RightButton onClick={handleComments}>
+                  <FiMessageSquare size="22" />
+                </RightButton>
               </Grid>
               <Grid item xs={2}>
                 {" "}
--- src/components/reviewCard/review-card.styles.jsx
+++ src/components/reviewCard/review-card.styles.jsx
@@ -1,5 +1,15 @@
 import styled from "styled-components";
+import Button from "@material-ui/core/Button";
 
 export const Wrap = styled.div`
   margin-top: 3%;
+`;
+
+export const Response = styled.div`
+  margin-top: 10%;
+  border-style: groove;
+`;
+
+export const RightButton = styled(Button)`
+  float: right;
 `;
