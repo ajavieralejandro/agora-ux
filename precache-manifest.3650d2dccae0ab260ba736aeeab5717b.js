d94d2f198d0cb93b9646c2446b8169bb0d0c1fde
ajavieralejandro <amorosijavier@gmail.com>
2020-04-29 00:33:33 -0300

    Updates

---
+++ precache-manifest.3650d2dccae0ab260ba736aeeab5717b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "37e5e8e9435a7703b8d1f4e8768508fd",
+    "url": "/agora-ux/index.html"
+  },
+  {
+    "revision": "4856583b8e9772162f04",
+    "url": "/agora-ux/static/css/main.b8f5bf34.chunk.css"
+  },
+  {
+    "revision": "20a604d68dd77855e832",
+    "url": "/agora-ux/static/js/2.6cefac27.chunk.js"
+  },
+  {
+    "revision": "c7e0c234cceaebad7e5cb36696a7d8ab",
+    "url": "/agora-ux/static/js/2.6cefac27.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4856583b8e9772162f04",
+    "url": "/agora-ux/static/js/main.ace6af17.chunk.js"
+  },
+  {
+    "revision": "4ad16fc8a177f78db11d",
+    "url": "/agora-ux/static/js/runtime-main.8d06e4ab.js"
+  }
+]);
